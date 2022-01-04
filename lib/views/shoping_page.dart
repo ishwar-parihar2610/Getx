53dd56cb1877e73c51c087eee3bcd96a4e31effe
ishwar parihar <paruharishwer26102000@gmail.com>
2022-01-04 20:56:34 +0530

    hello

--- lib/views/shoping_page.dart
+++ lib/views/shoping_page.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:state_managment/controllers/cart_controller.dart';
 import 'package:state_managment/controllers/shopping_controller.dart';
 
 class ShoppingPage extends StatelessWidget {
   final shoppingController = Get.put(ShoppingController());
+  final cartController = Get.put(CartController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {},
+            icon: Icon(
+              Icons.add_shopping_cart_rounded,
+              color: Colors.black,
+            ),
+            backgroundColor: Colors.amber,
+            label: GetX<CartController>(builder: (controller) {
+              return Text(controller.count.toString(),style: TextStyle(color: Colors.black,fontSize: 24),);
+            })),
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
@@ -46,8 +58,12 @@ class ShoppingPage extends StatelessWidget {
                                   ],
                                 ),
                                 ElevatedButton(
-                                    onPressed: () {},
-                                    child: Text("Add To cart"))
+                                  onPressed: () {
+                                    cartController
+                                        .addToCart(controller.products[index]);
+                                  },
+                                  child: Text("Add To cart"),
+                                )
                               ],
                             ),
                           ),
@@ -55,7 +71,14 @@ class ShoppingPage extends StatelessWidget {
                       });
                 }),
               ),
-              Text('Totol Amount: '),
+              GetX<CartController>(builder: (controller) {
+                return Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Text(
+                      'Totol Amount: \$ ${controller.totalPrice}',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ));
+              }),
               SizedBox(
                 height: 100,
               )
